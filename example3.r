1206e77ef9138cb6684006cf11eefb3e0e67f0ce
Kijoung Song <kys21207@gmail.com>
2025-03-22 13:26:04 -0400

    Update example3.r

--- example3.r
+++ example3.r
@@ -4,10 +4,12 @@ BiocManager::install("ensembldb")
 BiocManager::install("EnsDb.Hsapiens.v86")
 
 install.packages("locuszoomr")
+
 library(locuszoomr)
 library(data.table)
+library(EnsDb.Hsapiens.v86)
 
-source("scatter_plot_with_cs_pip.R")
+source("scatter_plot_with_cs.R")
 
 input_file1="/mnt/project/publically_available_supporting_files/gwas_public_results/meta_mvp_ukb_psoriasis.regenie.tsv.gz"
 data1 <- fread(cmd = paste("gunzip -c", shQuote(input_file1)), sep = "\t", header = TRUE)
@@ -30,5 +32,14 @@ merged_data2 <- merge(merged_data1, cs, by.x="ID", by.y="SNP",all.x=T)
 
 #CHROM GENPOS ID ALLELE0 ALLELE1 A1FREQ N BETA SE CHISQ LOG10P INFO
 dta <- merged_data2[, .(chrom = CHROM, pos = GENPOS, other_allele = ALLELE0, effect_allele = ALLELE1, 
-                    beta = BETA, se = SE, p = 10^(-LOG10P), rsid = rsID,cs_num = CS_Number, PIP)]
- 
+                    beta = BETA, se = SE, p = 10^(-LOG10P), rsid = rsID,cs_num = CS_Number)]
+loc1 <- locus(data = dta, gene = 'IL23R', flank = 1e5,
+             ens_db = "EnsDb.Hsapiens.v86") 
+pf <- quote({
+  v <- loc1$TX[loc1$TX$gene_name == "IL23R", c("start", "end")]
+  abline(v = v, col = "green")
+})
+oldpar <- set_layers(1)
+scatter_plot_cs(loc1,panel.first = pf)
+genetracks(loc1)
+par(oldpar)
